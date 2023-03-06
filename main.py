962626493b2645a88618a39179cbf8ee7ad1639d
CalvinYrd <calvin59600@gmail.com>
2023-03-07 00:41:19 +0100

    dfdf

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from pytube import YouTube
-import os
+import os, instaloader
 
 if (os.name == "nt"):
 	clear = lambda: os.system("cls")
@@ -7,18 +7,57 @@ else:
 	clear = lambda: os.system("clear")
 
 clear()
+link = 'https://www.instagram.com/reel/CmBJpVaP3VO/?igshid=YmMyMTA2M2Y='
 
 """
-youtube
-https://www.youtube.com/shorts/IGvQY07a6g8
-https://youtu.be/K7lxu3-_2vw
-https://www.youtube.com/watch?v=vSyOk_24Vy0
-"""
+# youtube
+# https://www.youtube.com/shorts/IGvQY07a6g8
+# https://youtu.be/K7lxu3-_2vw
+# https://www.youtube.com/watch?v=vSyOk_24Vy0
 
-link = 'https://www.youtube.com/watch?v=vSyOk_24Vy0'
+src = "youtube"
 yt = YouTube(link)
 
 stream = yt.streams.get_highest_resolution() # mp4
-# stream = yt.streams.filter(only_audio=True).first() # mp3
+ext = "mp4"
 
-stream.download()
+stream = yt.streams.filter(only_audio=True).first() # mp3
+ext = "mp3"
+
+title = stream.title
+"""
+
+# Insta
+# https://www.instagram.com/reel/CmBJpVaP3VO/?igshid=YmMyMTA2M2Y=
+
+src = "insta"
+title = "test"
+ext = "mp4"
+
+##############
+title += " "+src
+title = title.split(" ")
+for i in range(len(title)): title[i] = title[i].capitalize()
+title = "".join(title)
+
+filename = f"{title}.{ext}"
+output = f"downloads"
+##############
+
+"""
+# youtube
+stream.download(
+	output_path = output,
+	filename=filename
+)
+"""
+
+# insta
+code = link.split("instagram.com/reel/")[-1].split("/")[0]
+ig = instaloader.Instaloader()
+post = instaloader.Post.from_shortcode(ig.context, code)
+
+ig.download_post(
+	post = post,
+	target = output
+)
