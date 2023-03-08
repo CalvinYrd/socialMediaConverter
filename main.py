bd98c788e9cb4c12e7410e9dac366f2cf807ac92
unknown <calvin59600@gmail.com>
2023-03-08 20:04:29 +0100

    dgdfg

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 from pytube import YouTube
-import os, instaloader
+import os, instaloader, colorama
 
+# fonction pour clear le terminal
 if (os.name == "nt"):
 	clear = lambda: os.system("cls")
 else:
 	clear = lambda: os.system("clear")
 
 clear()
-link = 'https://www.instagram.com/reel/CmBJpVaP3VO/?igshid=YmMyMTA2M2Y='
 
+# retourne le réseau social du lien, si celui-ci est parmis les réseaux sociaux acceptés
 def linkType(link):
 	link = link.lstrip("https://").lstrip("http://").lstrip("www.")
 	res = None
@@ -18,10 +19,74 @@ def linkType(link):
 			link.split("/")[-1].startswith("?igshid=") or
 			link.split("/")[-1].startswith("&igshid=")
 		)
-	): res = "ig"
+	): res = ("ig", "INSTAGRAM")
 	elif (
+		link.startswith("youtube.com/watch?v=") or
+		link.startswith("youtube.com/shorts/") or
+		link.startswith("youtu.be/")
+	): res = ("yt", "YOUTUBE")
+	return res
 
-	)
+# si le fichier links n'existe pas on le créé
+filename = "links"
+if (not os.path.exists(filename)):
+	with open(filename, "w", encoding = "utf8") as file: pass
+	print(f'Un fichier du nom de "{filename}" a été créé')
+
+else:
+	newFileContent = ""
+	doneLinks = []
+
+	with open(filename, "r", encoding = "utf8") as file:
+		for link in file.readlines():
+			try:
+				src, fullSrc = linkType(link)
+				output = "downloads"
+
+				if (link.strip() and src and link not in doneLinks):
+					if (not link.startswith("https://")): link = "https://"+link
+					doneLinks.append(link)
+
+					if (src == "yt"):
+						yt = YouTube(link)
+						stream = yt.streams.get_highest_resolution() # mp4
+
+						title = stream.title
+						title = title.split(" ")
+						for i in range(len(title)): title[i] = title[i].capitalize()
+						title = "".join(title)
+						filename = f"{title}.mp4"
+
+						print(fullSrc)
+						stream.download(
+							output_path = output,
+							filename=filename
+						)
+						print((output+"/"+filename).replace("/", "\\"))
+
+					elif (src == "ig"):
+						code = link.split("instagram.com/reel/")[-1].split("/")[0]
+						ig = instaloader.Instaloader()
+						post = instaloader.Post.from_shortcode(ig.context, code)
+
+						print(fullSrc)
+						ig.download_post(
+							post = post,
+							target = output
+						)
+						for dl in os.listdir(output):
+							if (dl.endswith(".jpg") or dl.endswith(".json.xz")):
+								os.remove(output+"/"+dl)
+
+			except:
+				newFileContent += link+"\n"
+
+	clear()
+	n = len([line for line in newFileContent.split("\n") if line.strip()])
+	print(f"Résultat : {colorama.Fore.RED}{n}{colorama.Fore.RESET} lien(s) incorrect(s)")
+
+	with open(filename, "w", encoding = "utf8") as finalFile:
+		finalFile.writelines(newFileContent)
 
 """
 # youtube
@@ -29,7 +94,6 @@ def linkType(link):
 # https://youtu.be/K7lxu3-_2vw
 # https://www.youtube.com/watch?v=vSyOk_24Vy0
 
-src = "youtube"
 yt = YouTube(link)
 
 stream = yt.streams.get_highest_resolution() # mp4
@@ -47,7 +111,6 @@ title = stream.title
 ##############
 # cas youtube
 """
-title += " "+src
 title = title.split(" ")
 for i in range(len(title)): title[i] = title[i].capitalize()
 title = "".join(title)
